75cf98dd2d3184d4b20d01d156c8480ef4b96fef
Your Name <you@example.com>
2016-12-30 02:22:11 +0900

    add GraphicsContextExtension.fill*

--- src/main/scala/com/github/agetakoyaki29/scalafx/scene/canvas/GraphicsContextExtension.scala
+++ src/main/scala/com/github/agetakoyaki29/scalafx/scene/canvas/GraphicsContextExtension.scala
@@ -8,10 +8,15 @@ import javafx.scene.shape.ArcType
 object XXX {
 
   implicit class GraphicsContextExtension(that : GraphicsContext) {
-    def strokeLine(pt1: Point2D, pt2: Point2D) = that.strokeLine(pt1.getX, pt1.getY, pt2.getX, pt2.getY)
-    
     def fillCircle(x: Double, y: Double, w: Double, h: Double) = that.fillArc(x, y, w, h, 0, 360, ArcType.OPEN)
     def strokeCircle(x: Double, y: Double, w: Double, h: Double) = that.strokeArc(x, y, w, h, 0, 360, ArcType.OPEN)
+    
+    // ----
+    
+    def strokeLine2(pt1: Point2D, pt2: Point2D) = that.strokeLine(pt1.getX, pt1.getY, pt2.getX, pt2.getY)
+    
+    def strokeCircle2(pt1: Point2D, pt2: Point2D) = this.strokeCircle(pt1.getX, pt1.getY, pt2.getX, pt2.getY)
+    def fillCircle2(pt1: Point2D, pt2: Point2D) = this.fillCircle(pt1.getX, pt1.getY, pt2.getX, pt2.getY)
   }
   
 }
