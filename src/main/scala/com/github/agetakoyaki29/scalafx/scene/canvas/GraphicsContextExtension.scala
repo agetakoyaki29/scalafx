2909dbc9e537cc54e8fdc6b1bab1a0cb17d3cc7d
Your Name <you@example.com>
2017-01-10 11:54:12 +0900

    rename XXX func args

--- src/main/scala/com/github/agetakoyaki29/scalafx/scene/canvas/GraphicsContextExtension.scala
+++ src/main/scala/com/github/agetakoyaki29/scalafx/scene/canvas/GraphicsContextExtension.scala
@@ -10,13 +10,13 @@ object XXX {
   implicit class GraphicsContextExtension(that : GraphicsContext) {
     def fillCircle(x: Double, y: Double, w: Double, h: Double) = that.fillArc(x, y, w, h, 0, 360, ArcType.OPEN)
     def strokeCircle(x: Double, y: Double, w: Double, h: Double) = that.strokeArc(x, y, w, h, 0, 360, ArcType.OPEN)
-    
+
     // ----
-    
-    def strokeLine2(pt1: Point2D, pt2: Point2D) = that.strokeLine(pt1.getX, pt1.getY, pt2.getX, pt2.getY)
-    
-    def strokeCircle2(pt1: Point2D, pt2: Point2D) = this.strokeCircle(pt1.getX, pt1.getY, pt2.getX, pt2.getY)
-    def fillCircle2(pt1: Point2D, pt2: Point2D) = this.fillCircle(pt1.getX, pt1.getY, pt2.getX, pt2.getY)
+
+    def strokeLine2(sp: Point2D, ep: Point2D) = that.strokeLine(sp.getX, sp.getY, ep.getX, ep.getY)
+
+    def strokeCircle2(sp: Point2D, size: Point2D) = this.strokeCircle(sp.getX, sp.getY, size.getX, size.getY)
+    def fillCircle2(sp: Point2D, size: Point2D) = this.fillCircle(sp.getX, sp.getY, size.getX, size.getY)
   }
-  
+
 }
